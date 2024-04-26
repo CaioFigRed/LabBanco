72920e66d1046a133fa190b0067b3c04b191e1b8
Caio Figueiredo <114994693+CaioFigRed@users.noreply.github.com>
2024-04-26 14:37:36 -0300

    removido entidade prouni

--- src/main/java/com/example/labbanco/repositories/ProuniRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.labbanco.repositories;
-
-import com.example.labbanco.entities.Prouni;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProuniRepository extends JpaRepository<Prouni, Long> {
-}
